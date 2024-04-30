e1d1278ede3ffcb71600504e8e736858ba55a407
Kafui Alordo <kalordo7@gmail.com>
2024-04-30 02:35:44 +0000

    handle errors

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,6 @@
-import { deleteInvoice } from '/app/lib/actions';
 import Link from 'next/link';
 
+import { deleteInvoice } from '@/app/lib/actions';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 export function CreateInvoice() {
